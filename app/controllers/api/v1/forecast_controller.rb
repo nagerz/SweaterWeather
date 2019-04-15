ed96286d4ab677ef50460014331c6dc877e0ef17
nagerz <znager@gmail.com>
2019-04-15 15:34:00 -0600

    cleaned out commented code

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -10,36 +10,4 @@ class Api::V1::ForecastController < ApplicationController
     params.permit(:location)
   end
 
-  # def geolocation
-  #   geocode_service.geocode(search_params[:location])
-  # end
-  #
-  # def geodata
-  #   data = {}
-  #   data[:geo_lat] = geolocation[:results][0][:geometry][:location][:lat]
-  #   data[:geo_long] = geolocation[:results][0][:geometry][:location][:lng]
-  #   data[:geo_city] = geolocation[:results][0][:address_components][0][:long_name]
-  #   data[:geo_state] = geolocation[:results][0][:address_components][2][:short_name]
-  #   data
-  # end
-  #
-  # def search_city(data)
-  #   City.find_or_create_by(lat: data[:geo_lat], long: data[:geo_long]) do |city|
-  #     city.city = data[:geo_city]
-  #     city.state = data[:geo_state]
-  #   end
-  # end
-  #
-  # def forecast(city)
-  #   Forecast.new(weather_service.get_forecast(city), city)
-  # end
-  #
-  # def weather_service
-  #   DarkskyService.new
-  # end
-  #
-  # def geocode_service
-  #   GoogleMapsService.new
-  # end
-
 end
